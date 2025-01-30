918f80dcaf0fc38cdeba8e4d0cc995624a413c0e
Daniel <32326750+DaniGwen@users.noreply.github.com>
2025-01-30 18:31:24 +0200

    PositionRightArm hardcoded values

--- Framework/src/gripper/GripperControl.cpp
+++ Framework/src/gripper/GripperControl.cpp
@@ -69,13 +69,13 @@ void CenterWrist(CM730 &cm730)
 // Arm Positioning
 void PositionRightArm(CM730 &cm730)
 {
-    cm730.WriteWord(JointData::ID_R_SHOULDER_PITCH, MX28::P_GOAL_POSITION_L, 1500, 0);
+    cm730.WriteWord(JointData::ID_R_SHOULDER_PITCH, MX28::P_GOAL_POSITION_L, 2400, 0);
     WaitWhileServoMoving(cm730, JointData::ID_R_SHOULDER_PITCH);
 
-    cm730.WriteWord(JointData::ID_R_SHOULDER_ROLL, MX28::P_GOAL_POSITION_L, 2000, 0);
+    cm730.WriteWord(JointData::ID_R_SHOULDER_ROLL, MX28::P_GOAL_POSITION_L, 1850, 0);
     WaitWhileServoMoving(cm730, JointData::ID_R_SHOULDER_ROLL);
 
-    cm730.WriteWord(JointData::ID_R_ELBOW, MX28::P_GOAL_POSITION_L, 2000, 0);
+    cm730.WriteWord(JointData::ID_R_ELBOW, MX28::P_GOAL_POSITION_L, 2400, 0);
     WaitWhileServoMoving(cm730, JointData::ID_R_ELBOW);
 }
 
