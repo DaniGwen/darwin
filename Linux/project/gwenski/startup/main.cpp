c1b9d7cfdaa24e4d0737f73bc892abadfbfd7ad6
Daniel <32326750+DaniGwen@users.noreply.github.com>
2025-02-07 20:09:30 +0200

    blue

--- Linux/project/gwenski/startup/main.cpp
+++ Linux/project/gwenski/startup/main.cpp
@@ -8,7 +8,6 @@
 #include "LinuxDARwIn.h"
 
 #define INI_FILE_PATH "config.ini"
-
 #define U2D_DEV_NAME "/dev/ttyUSB0"
 
 int main()
@@ -21,9 +20,12 @@ int main()
     LinuxCamera::GetInstance()->Initialize(0);
     LinuxCamera::GetInstance()->LoadINISettings(ini);
 
-    ColorFinder *ball_finder = new ColorFinder();
-    ball_finder->LoadINISettings(ini);
-    httpd::ball_finder = ball_finder;
+    ColorFinder *red_ball_finder = new ColorFinder();
+    red_ball_finder->LoadINISettings(ini);
+    httpd::ball_finder = red_ball_finder;
+
+    ColorFinder *blue_ball_finder = new ColorFinder();
+    blue_ball_finder->LoadINISettings(ini, "Blue_Ball");
 
     BallTracker tracker = BallTracker();
 
@@ -50,35 +52,48 @@ int main()
     Head::GetInstance()->m_Joint.SetPGain(JointData::ID_HEAD_PAN, 8);
     Head::GetInstance()->m_Joint.SetPGain(JointData::ID_HEAD_TILT, 8);
 
-    // Change Eyes color to red
-    cm730.WriteWord(CM730::ID_CM, CM730::P_LED_EYE_L, cm730.MakeColor(255, 255, 255), 0);
-
-    LinuxActionScript::PlayMP3("../../../../Data/mp3/woman-ya.mp3");
+    LinuxActionScript::PlayMP3("../../../../Data/mp3/lock-and-load-male.mp3");
 
     while (1)
     {
         LinuxCamera::GetInstance()->CaptureFrame();
 
-        tracker.Process(ball_finder->GetPosition(LinuxCamera::GetInstance()->fbuffer->m_HSVFrame));
-        bool colorfound = false;
+        tracker.Process(red_ball_finder->GetPosition(LinuxCamera::GetInstance()->fbuffer->m_HSVFrame));
+        tracker.Process(blue_ball_finder->GetPosition(LinuxCamera::GetInstance()->fbuffer->m_HSVFrame));
+
+        bool red_found = false;
+        bool blue_found = false;
         rgb_ball = LinuxCamera::GetInstance()->fbuffer->m_RGBFrame;
         for (int i = 0; i < rgb_ball->m_NumberOfPixels; i++)
         {
-            if (ball_finder->m_result->m_ImageData[i] == 1)
+            if (red_ball_finder->m_result->m_ImageData[i] == 1)
             {
                 rgb_ball->m_ImageData[i * rgb_ball->m_PixelSize + 0] = 255;
                 rgb_ball->m_ImageData[i * rgb_ball->m_PixelSize + 1] = 0;
                 rgb_ball->m_ImageData[i * rgb_ball->m_PixelSize + 2] = 0;
-                colorfound = true;
+                red_found = true;
+            }
+            if (blue_ball_finder->m_result->m_ImageData[i] == 1)
+            {
+                rgb_ball->m_ImageData[i * rgb_ball->m_PixelSize + 0] = 0;
+                rgb_ball->m_ImageData[i * rgb_ball->m_PixelSize + 1] = 0;
+                rgb_ball->m_ImageData[i * rgb_ball->m_PixelSize + 2] = 255;
+                blue_found = true;
             }
         }
 
-        if (colorfound)
+        // Set LED eyes to red if the red ball is found, blue if the blue ball is found
+        if (red_found)
         {
             cm730.WriteWord(CM730::ID_CM, CM730::P_LED_EYE_L, cm730.MakeColor(255, 0, 0), 0);
         }
-        else{
-             cm730.WriteWord(CM730::ID_CM, CM730::P_LED_EYE_L, cm730.MakeColor(255, 255, 255), 0);
+        else if (blue_found)
+        {
+            cm730.WriteWord(CM730::ID_CM, CM730::P_LED_EYE_L, cm730.MakeColor(0, 0, 255), 0);
+        }
+        else
+        {
+            cm730.WriteWord(CM730::ID_CM, CM730::P_LED_EYE_L, cm730.MakeColor(255, 255, 255), 0);
         }
     }
 
