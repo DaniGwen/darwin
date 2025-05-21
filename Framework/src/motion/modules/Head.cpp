42c9b66344e622984db2d1c680b84523d8211150
DaniGwen <dani.gwenski@gmail.com>
2025-05-21 21:04:20 +0300

    .

--- Framework/src/motion/modules/Head.cpp
+++ Framework/src/motion/modules/Head.cpp
@@ -1,15 +1,17 @@
 /*
- *   Head.cpp
+ * Head.cpp
  *
- *   Author: ROBOTIS
+ * Author: ROBOTIS
  *
  */
 
 #include <stdio.h>
+#include <iostream> // For std::cout, std::cerr, std::endl
 #include "MX28.h"
 #include "Kinematics.h"
 #include "MotionStatus.h"
 #include "Head.h"
+
 
 using namespace Robot;
 
@@ -18,21 +20,21 @@ Head* Head::m_UniqueInstance = new Head();
 
 Head::Head()
 {
-	m_Pan_p_gain = 0.1;
-	m_Pan_d_gain = 0.22;
+    m_Pan_p_gain = 0.1;
+    m_Pan_d_gain = 0.22;
 
     m_Tilt_p_gain = 0.1;
-	m_Tilt_d_gain = 0.22;
+    m_Tilt_d_gain = 0.22;
 
-	m_LeftLimit = 70;
-	m_RightLimit = -70;
-	m_TopLimit = Kinematics::EYE_TILT_OFFSET_ANGLE;
-	m_BottomLimit = Kinematics::EYE_TILT_OFFSET_ANGLE - 65;
+    m_LeftLimit = 70;
+    m_RightLimit = -70;
+    m_TopLimit = Kinematics::EYE_TILT_OFFSET_ANGLE;
+    m_BottomLimit = Kinematics::EYE_TILT_OFFSET_ANGLE - 65;
 
-	m_Pan_Home = 0.0;
-	m_Tilt_Home = Kinematics::EYE_TILT_OFFSET_ANGLE - 30.0;
+    m_Pan_Home = 0.0;
+    m_Tilt_Home = Kinematics::EYE_TILT_OFFSET_ANGLE - 30.0;
 
-	m_Joint.SetEnableHeadOnly(true);
+    m_Joint.SetEnableHeadOnly(true); // This should be handled by HeadTracking::Initialize
 }
 
 Head::~Head()
@@ -41,136 +43,126 @@ Head::~Head()
 
 void Head::CheckLimit()
 {
-	if(m_PanAngle > m_LeftLimit)
-		m_PanAngle = m_LeftLimit;
-	else if(m_PanAngle < m_RightLimit)
-		m_PanAngle = m_RightLimit;
+    if(m_PanAngle > m_LeftLimit)
+        m_PanAngle = m_LeftLimit;
+    else if(m_PanAngle < m_RightLimit)
+        m_PanAngle = m_RightLimit;
 
-	if(m_TiltAngle > m_TopLimit)
-		m_TiltAngle = m_TopLimit;
-	else if(m_TiltAngle < m_BottomLimit)
-		m_TiltAngle = m_BottomLimit;	
-}
+    if(m_TiltAngle > m_TopLimit)
+        m_TiltAngle = m_TopLimit;
+    else if(m_TiltAngle < m_BottomLimit)
+        m_TiltAngle = m_BottomLimit;
 
-void Head::Initialize()
-{
-	m_PanAngle = MotionStatus::m_CurrentJoints.GetAngle(JointData::ID_HEAD_PAN);
-	m_TiltAngle = -MotionStatus::m_CurrentJoints.GetAngle(JointData::ID_HEAD_TILT);
-	CheckLimit();
-
-	InitTracking();
-	MoveToHome();
+    // DEBUG: Print angles after limit check
+    // std::cout << "DEBUG: Head::CheckLimit - PanAngle: " << m_PanAngle << ", TiltAngle: " << m_TiltAngle << std::endl;
 }
 
 void Head::LoadINISettings(minIni* ini)
 {
-    LoadINISettings(ini, HEAD_SECTION);
-}
+    m_Pan_p_gain = ini->getd("Head Pan/Tilt", "Pan_P_GAIN", m_Pan_p_gain);
+    m_Pan_d_gain = ini->getd("Head Pan/Tilt", "Pan_D_GAIN", m_Pan_d_gain);
+    m_Tilt_p_gain = ini->getd("Head Pan/Tilt", "Tilt_P_GAIN", m_Tilt_p_gain);
+    m_Tilt_d_gain = ini->getd("Head Pan/Tilt", "Tilt_D_GAIN", m_Tilt_d_gain);
 
-void Head::LoadINISettings(minIni* ini, const std::string &section)
-{
-    double value = INVALID_VALUE;
+    m_LeftLimit = ini->getd("Head Pan/Tilt", "LeftLimit", m_LeftLimit);
+    m_RightLimit = ini->getd("Head Pan/Tilt", "RightLimit", m_RightLimit);
+    m_TopLimit = ini->getd("Head Pan/Tilt", "TopLimit", m_TopLimit);
+    m_BottomLimit = ini->getd("Head Pan/Tilt", "BottomLimit", m_BottomLimit);
 
-    if((value = ini->getd(section, "pan_p_gain", INVALID_VALUE)) != INVALID_VALUE)  m_Pan_p_gain = value;
-    if((value = ini->getd(section, "pan_d_gain", INVALID_VALUE)) != INVALID_VALUE)  m_Pan_d_gain = value;
-    if((value = ini->getd(section, "tilt_p_gain", INVALID_VALUE)) != INVALID_VALUE) m_Tilt_p_gain = value;
-    if((value = ini->getd(section, "tilt_d_gain", INVALID_VALUE)) != INVALID_VALUE) m_Tilt_d_gain = value;
-    if((value = ini->getd(section, "left_limit", INVALID_VALUE)) != INVALID_VALUE)  m_LeftLimit = value;
-    if((value = ini->getd(section, "right_limit", INVALID_VALUE)) != INVALID_VALUE) m_RightLimit = value;
-    if((value = ini->getd(section, "top_limit", INVALID_VALUE)) != INVALID_VALUE)   m_TopLimit = value;
-    if((value = ini->getd(section, "bottom_limit", INVALID_VALUE)) != INVALID_VALUE)m_BottomLimit = value;
-    if((value = ini->getd(section, "pan_home", INVALID_VALUE)) != INVALID_VALUE)    m_Pan_Home = value;
-    if((value = ini->getd(section, "tilt_home", INVALID_VALUE)) != INVALID_VALUE)   m_Tilt_Home = value;
-}
+    m_Pan_Home = ini->getd("Head Pan/Tilt", "Pan_Home", m_Pan_Home);
+    m_Tilt_Home = ini->getd("Head Pan/Tilt", "Tilt_Home", m_Tilt_Home);
 
-void Head::SaveINISettings(minIni* ini)
-{
-    SaveINISettings(ini, HEAD_SECTION);
-}
-
-void Head::SaveINISettings(minIni* ini, const std::string &section)
-{
-    ini->put(section,   "pan_p_gain",   m_Pan_p_gain);
-    ini->put(section,   "pan_d_gain",   m_Pan_d_gain);
-    ini->put(section,   "tilt_p_gain",  m_Tilt_p_gain);
-    ini->put(section,   "tilt_d_gain",  m_Tilt_d_gain);
-    ini->put(section,   "left_limit",   m_LeftLimit);
-    ini->put(section,   "right_limit",  m_RightLimit);
-    ini->put(section,   "top_limit",    m_TopLimit);
-    ini->put(section,   "bottom_limit", m_BottomLimit);
-    ini->put(section,   "pan_home",     m_Pan_Home);
-    ini->put(section,   "tilt_home",    m_Tilt_Home);
+    std::cout << "INFO: Head::LoadINISettings - Pan_P_GAIN: " << m_Pan_p_gain
+              << ", Pan_D_GAIN: " << m_Pan_d_gain
+              << ", Tilt_P_GAIN: " << m_Tilt_p_gain
+              << ", Tilt_D_GAIN: " << m_Tilt_d_gain << std::endl;
+    std::cout << "INFO: Head::LoadINISettings - Limits: L=" << m_LeftLimit << ", R=" << m_RightLimit
+              << ", T=" << m_TopLimit << ", B=" << m_BottomLimit << std::endl;
+    std::cout << "INFO: Head::LoadINISettings - Home: Pan=" << m_Pan_Home << ", Tilt=" << m_Tilt_Home << std::endl;
 }
 
 void Head::MoveToHome()
 {
-	MoveByAngle(m_Pan_Home, m_Tilt_Home);
+    MoveByAngle(m_Pan_Home, m_Tilt_Home);
+    std::cout << "DEBUG: Head::MoveToHome - Moving to Pan_Home: " << m_Pan_Home << ", Tilt_Home: " << m_Tilt_Home << std::endl;
 }
 
 void Head::MoveByAngle(double pan, double tilt)
 {
-	m_PanAngle = pan;
-	m_TiltAngle = tilt;
+    m_PanAngle = pan;
+    m_TiltAngle = tilt;
 
-	CheckLimit();
+    CheckLimit();
+    std::cout << "DEBUG: Head::MoveByAngle - Target Pan: " << pan << ", Target Tilt: " << tilt
+              << " (After Limit: Pan: " << m_PanAngle << ", Tilt: " << m_TiltAngle << ")" << std::endl;
 }
 
 void Head::MoveByAngleOffset(double pan, double tilt)
-{	
-	MoveByAngle(m_PanAngle + pan, m_TiltAngle + tilt);
+{
+    MoveByAngle(m_PanAngle + pan, m_TiltAngle + tilt);
+    std::cout << "DEBUG: Head::MoveByAngleOffset - Offset Pan: " << pan << ", Offset Tilt: " << tilt << std::endl;
 }
 
 void Head::InitTracking()
 {
-	m_Pan_err = 0;
-	m_Pan_err_diff = 0;
-	m_Tilt_err = 0;
-	m_Tilt_err_diff = 0;
+    m_Pan_err = 0;
+    m_Pan_err_diff = 0;
+    m_Tilt_err = 0;
+    m_Tilt_err_diff = 0;
+    std::cout << "DEBUG: Head::InitTracking - Tracking errors reset." << std::endl;
 }
 
 void Head::MoveTracking(Point2D err)
-{	
-	m_Pan_err_diff = err.X - m_Pan_err;
-	m_Pan_err = err.X;
+{
+    m_Pan_err_diff = err.X - m_Pan_err;
+    m_Pan_err = err.X;
 
-	m_Tilt_err_diff = err.Y - m_Tilt_err;
-	m_Tilt_err = err.Y;
+    m_Tilt_err_diff = err.Y - m_Tilt_err;
+    m_Tilt_err = err.Y;
 
-	MoveTracking();
+    std::cout << "DEBUG: Head::MoveTracking(Point2D err) - Input P_err.X: " << err.X << ", P_err.Y: " << err.Y << std::endl;
+    std::cout << "DEBUG: Head::MoveTracking(Point2D err) - m_Pan_err: " << m_Pan_err << ", m_Pan_err_diff: " << m_Pan_err_diff << std::endl;
+    std::cout << "DEBUG: Head::MoveTracking(Point2D err) - m_Tilt_err: " << m_Tilt_err << ", m_Tilt_err_diff: " << m_Tilt_err_diff << std::endl;
+
+    MoveTracking(); // Call the overloaded version
 }
 
 void Head::MoveTracking()
 {
-	double pOffset, dOffset;
+    double pOffset, dOffset;
 
-	pOffset = m_Pan_err * m_Pan_p_gain;
-	pOffset *= pOffset;
-	if(m_Pan_err < 0)
-		pOffset = -pOffset;
-	dOffset = m_Pan_err_diff * m_Pan_d_gain;
-	dOffset *= dOffset;
-	if(m_Pan_err_diff < 0)
-		dOffset = -dOffset;
-	m_PanAngle += (pOffset + dOffset);
+    // Pan PID calculation
+    pOffset = m_Pan_err * m_Pan_p_gain;
+    dOffset = m_Pan_err_diff * m_Pan_d_gain;
+    m_PanAngle += (pOffset + dOffset);
 
-	pOffset = m_Tilt_err * m_Tilt_p_gain;
-	pOffset *= pOffset;
-	if(m_Tilt_err < 0)
-		pOffset = -pOffset;
-	dOffset = m_Tilt_err_diff * m_Tilt_d_gain;
-	dOffset *= dOffset;
-	if(m_Tilt_err_diff < 0)
-		dOffset = -dOffset;
-	m_TiltAngle += (pOffset + dOffset);
+    // Tilt PID calculation
+    pOffset = m_Tilt_err * m_Tilt_p_gain;
+    dOffset = m_Tilt_err_diff * m_Tilt_d_gain;
+    m_TiltAngle += (pOffset + dOffset);
 
-	CheckLimit();
+    std::cout << "DEBUG: Head::MoveTracking() - Pan pOffset: " << m_Pan_err * m_Pan_p_gain
+              << ", Pan dOffset: " << m_Pan_err_diff * m_Pan_d_gain
+              << ", New PanAngle (before limit): " << m_PanAngle << std::endl;
+    std::cout << "DEBUG: Head::MoveTracking() - Tilt pOffset: " << m_Tilt_err * m_Tilt_p_gain
+              << ", Tilt dOffset: " << m_Tilt_err_diff * m_Tilt_d_gain
+              << ", New TiltAngle (before limit): " << m_TiltAngle << std::endl;
+
+    CheckLimit(); // Apply limits after calculating new angles
 }
 
 void Head::Process()
 {
-	if(m_Joint.GetEnable(JointData::ID_HEAD_PAN) == true)
-		m_Joint.SetAngle(JointData::ID_HEAD_PAN, m_PanAngle);
+    // Check if Head joints are enabled by checking individual joint enable status
+    // This assumes JointData::GetEnable(id) is available and returns true if enabled.
+    if(m_Joint.GetEnable(JointData::ID_HEAD_PAN) && m_Joint.GetEnable(JointData::ID_HEAD_TILT)){
+        // Set target angles for the head motors
+        m_Joint.SetAngle(JointData::ID_HEAD_PAN, m_PanAngle);
+        m_Joint.SetAngle(JointData::ID_HEAD_TILT, m_TiltAngle);
 
-	if(m_Joint.GetEnable(JointData::ID_HEAD_TILT) == true)
-		m_Joint.SetAngle(JointData::ID_HEAD_TILT, m_TiltAngle);
+        std::cout << "DEBUG: Head::Process - Setting Pan Angle: " << m_PanAngle
+                  << ", Tilt Angle: " << m_TiltAngle << std::endl;
+    } else {
+        std::cout << "DEBUG: Head::Process - Head joints are not enabled, skipping angle setting." << std::endl;
+    }
 }
