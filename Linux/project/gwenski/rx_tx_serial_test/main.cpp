54f0b558d672f8de6f1eee0a5a44f7be10f8fc0a
DaniGwen <dani.gwenski@gmail.com>
2025-05-21 21:52:55 +0300

    test

--- Linux/project/gwenski/rx_tx_serial_test/main.cpp
+++ Linux/project/gwenski/rx_tx_serial_test/main.cpp
@@ -22,7 +22,8 @@ int main()
 
     Head::GetInstance()->Initialize();
     Head::GetInstance()->m_Joint.SetEnableHeadOnly(true, true);
-    Head::GetInstance()->MoveByAngleOffset(-20, 20);
+    Head::GetInstance()->InitTracking();
+    Head::GetInstance()->MoveByAngle(10, -10);
 
     // cm730.WriteWord(CM730::ID_CM, CM730::P_LED_EYE_L, cm730.MakeColor(255, 0, 0), 0);
     // sleep(1); // Wait for 1 second
