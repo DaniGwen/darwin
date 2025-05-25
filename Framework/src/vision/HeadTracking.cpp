8c24c3a6037a9eda2de28d134cef4cf62470d8f8
DaniGwen <dani.gwenski@gmail.com>
2025-05-25 23:11:25 +0300

    .

--- Framework/src/vision/HeadTracking.cpp
+++ Framework/src/vision/HeadTracking.cpp
@@ -91,8 +91,8 @@ HeadTracking::HeadTracking()
       // Keep them at 1.0 unless you have a specific reason to scale the error itself.
       pan_error_scale_(1.0),
       tilt_error_scale_(1.0),
-      pan_deadband_deg_(1.0),
-      tilt_deadband_deg_(1.0),
+      pan_deadband_deg_(0.5),
+      tilt_deadband_deg_(0.5),
       black_color_(0),
       frame_counter_(0),
       current_detected_label_("none"),
