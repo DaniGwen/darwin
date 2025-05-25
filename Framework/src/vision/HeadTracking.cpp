33045b24070f1d1b9fac5e1c7af670edc40c9aba
DaniGwen <dani.gwenski@gmail.com>
2025-05-25 23:05:05 +0300

    .

--- Framework/src/vision/HeadTracking.cpp
+++ Framework/src/vision/HeadTracking.cpp
@@ -85,7 +85,7 @@ HeadTracking::HeadTracking()
       m_TopLimit(0.0),      // Will be set by Kinematics::EYE_TILT_OFFSET_ANGLE
       m_BottomLimit(-68.0), // Will be set by Kinematics::EYE_TILT_OFFSET_ANGLE
       m_Pan_Home(0.0),
-      m_Tilt_Home(-30.0), // Will be set by Kinematics::EYE_TILT_OFFSET_ANGLE
+      m_Tilt_Home(-20.0), // Will be set by Kinematics::EYE_TILT_OFFSET_ANGLE
       no_target_count_(0),
       // These scales multiply the error *before* applying P/D gains.
       // Keep them at 1.0 unless you have a specific reason to scale the error itself.
