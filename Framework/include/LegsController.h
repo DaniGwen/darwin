a271cefe167a122c38330d9382cb389542f1a831
DaniGwen <dani.gwenski@gmail.com>
2025-05-28 22:01:37 +0300

    .

--- Framework/include/LegsController.h
+++ Framework/include/LegsController.h
@@ -26,8 +26,8 @@ namespace Robot
         LegsController(CM730 *cm730);
         ~LegsController();
 
-        void Stand(int p_gain = 4);
-        void ReadyToPickUpItem(int p_gain = 4);
+        void Stand(int p_gain = 11);
+        void ReadyToPickUpItem(int p_gain = 11);
 
     private:
         CM730 *cm730_; // Pointer to the CM730 board interface
@@ -63,7 +63,7 @@ namespace Robot
                 {JointData::ID_L_ANKLE_ROLL, 1978}}};
 
         void ApplyPose(const Pose &pose);
-        void SetPID(int p_gain = 4);
+        void SetPID(int p_gain = 11);
     };
 }
 
