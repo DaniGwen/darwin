db6f6e73e72b087e3cbb81f47e9e583786053bc1
DaniGwen <dani.gwenski@gmail.com>
2025-05-28 20:17:19 +0300

    .

--- Framework/src/vision/HeadTracking.cpp
+++ Framework/src/vision/HeadTracking.cpp
@@ -8,6 +8,17 @@
  * Description: Implementation of the HeadTracking singleton class.
  * Directly controls head motors without relying on Head.cpp.
  */
+// Define some color codes for convenience
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
 
 #include "HeadTracking.h" // Include the corrected header first
 #include "Globals.h"
