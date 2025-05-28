32edf1e61d3439de8b8459707456590b44f9aa4e
DaniGwen <dani.gwenski@gmail.com>
2025-05-28 20:29:47 +0300

    .

--- Framework/include/ConsoleColors.h
+++ Framework/include/ConsoleColors.h
@@ -1,10 +1,31 @@
 // Define some color codes for convenience
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
+#define RESET "\033[0m"
+
+#define BLACK "\033[30m"   /* Black */
+#define RED "\033[31m"     /* Red */
+#define GREEN "\033[32m"   /* Green */
+#define YELLOW "\033[33m"  /* Yellow */
+#define BLUE "\033[34m"    /* Blue */
+#define MAGENTA "\033[35m" /* Magenta */
+#define CYAN "\033[36m"    /* Cyan */
+#define WHITE "\033[37m"   /* White */
+
+// Bold Colors
+#define BOLDBLACK "\033[1m\033[30m"   /* Bold Black */
+#define BOLDRED "\033[1m\033[31m"     /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m"   /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m"  /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m"    /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m"    /* Bold Cyan */
+#define BOLDWHITE "\033[1m\033[37m"   /* Bold White */
+
+// Underline Colors
+#define UBLACK "\033[4m\033[30m"   /* Underline Black */
+#define URED "\033[4m\033[31m"     /* Underline Red */
+#define UGREEN "\033[4m\033[32m"   /* Underline Green */
+#define UYELLOW "\033[4m\033[33m"  /* Underline Yellow */
+#define UBLUE "\033[4m\033[34m"    /* Underline Blue */
+#define UMAGENTA "\033[4m\033[35m" /* Underline Magenta */
+#define UCYAN "\033[4m\033[36m"    /* Underline Cyan */
+#define UWHITE "\033[4m\033[37m"   /* Underline White */
