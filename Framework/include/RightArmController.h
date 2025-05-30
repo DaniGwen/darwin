6113b5bbdfc4dbdaf5536dbd10dc1c495f71bb69
DaniGwen <dani.gwenski@gmail.com>
2025-05-30 21:00:57 +0300

    .

--- Framework/include/RightArmController.h
+++ Framework/include/RightArmController.h
@@ -28,7 +28,7 @@ namespace Robot
 
         const Pose POSE_REACH_HAND = {
             std::map<int, int>{
-                {JointData::ID_R_SHOULDER_PITCH, 1719},
+                {JointData::ID_R_SHOULDER_PITCH, 2610},
                 {JointData::ID_R_SHOULDER_ROLL, 2010},
                 {JointData::ID_R_ELBOW, 1470},
 
--- Linux/project/gwenski/react_on_object_detect/main.cpp
+++ Linux/project/gwenski/react_on_object_detect/main.cpp
@@ -124,7 +124,7 @@ void handleBottleDetected(LegsController &legs_controller,
     MotionManager::GetInstance()->AddModule(static_cast<MotionModule *>(Walking::GetInstance())); // Need to load the Walking module to MotionManager in order to use it in LegsController
     MotionManager::GetInstance()->SetEnable(true);                                                // Enable MotionManager to allow walking
 
-    legs_controller.TurnLeft(5);
+    legs_controller.TurnLeft();
     std::this_thread::sleep_for(std::chrono::milliseconds(450));
     legs_controller.StopWalk();
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
