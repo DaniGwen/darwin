a2e384787c76d6a23f1bada3ad417a6535345f27
DaniGwen <dani.gwenski@gmail.com>
2025-06-01 10:49:39 +0300

    .

--- Framework/src/vision/HeadTracking.cpp
+++ Framework/src/vision/HeadTracking.cpp
@@ -118,7 +118,7 @@ namespace Robot
           detection_score_(0),
           last_motor_command_time_(std::chrono::steady_clock::now()),
           motor_command_interval_ms_(100),
-          camera_focal_length_px_(700.0),
+          camera_focal_length_px_(200.0),
           current_object_distance_m_(-1.0),
           m_last_object_angular_error(-1.0, -1.0)
     {
@@ -943,8 +943,8 @@ namespace Robot
 
         for (int joint_id : joints)
         {
-            cm730_->WriteByte(joint_id, MX28::P_P_GAIN, 10, 0);
-            cm730_->WriteByte(joint_id, MX28::P_D_GAIN, 10, 0);
+            cm730_->WriteByte(joint_id, MX28::P_P_GAIN, 9, 0);
+            cm730_->WriteByte(joint_id, MX28::P_D_GAIN, 9, 0);
             cm730_->WriteWord(joint_id, MX28::P_MOVING_SPEED_L, 200, 0); // Value 0 means max speed. 1~1023 for controlled speed.
         }
     }
