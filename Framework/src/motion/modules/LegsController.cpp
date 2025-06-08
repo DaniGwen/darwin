4397a83ed9e7bc0fb9641fcadbf01c686ce394ea
Daniel <32326750+DaniGwen@users.noreply.github.com>
2025-06-08 18:51:02 +0300

    .

--- Framework/src/motion/modules/LegsController.cpp
+++ Framework/src/motion/modules/LegsController.cpp
@@ -118,6 +118,7 @@ namespace Robot
     {
         std::cout << BOLDGREEN << "INFO: Initializing Walking module through LegsController..." << RESET << std::endl;
         Walking::GetInstance()->Initialize();
+        Walking::GetInstance()->BALANCE_ENABLE = true;
         if (ini)
         {
             Walking::GetInstance()->LoadINISettings(ini, section);
